83ca0844eb946d5ceba86220c2adac3aa8e1d722
edeniz9191@gmail.com <edeniz9191@gmail.com>
2020-06-16 12:58:38 -0400

    create user model, cartedProduct model, associations, and login route.

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,6 @@
 const User = require('./user')
+const CartedProduct = require('/cartedProduct')
+const Product = require('/product')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -13,6 +15,13 @@ const User = require('./user')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+ User.hasMany(CartedProduct)
+ CartedProduct.belongsTo(User)
+ CartedProduct.hasOne(Product)
+
 module.exports = {
-  User
+  User,
+  CartedProduct,
+  Product
 }

79749f4f1d5872b95d7ee80a4168f6795ae3487d
Connie Lok <conniehlok@gmail.com>
2020-06-16 13:04:36 -0400

    product specs

---
+++ server/api/products.spec.js
@@ -0,0 +1,40 @@
+/* global describe beforeEach it */
+
+const {expect} = require('chai');
+const request = require('supertest');
+const db = require('../db');
+const app = require('../index');
+// const Product = db.model('product');
+const Product = require('../db/models/product');
+
+describe('Product routes', () => {
+  beforeEach(() => {
+    return db.sync({force: true});
+  });
+
+  describe('/api/products/', () => {
+    const toiletPaper = {
+      name: 'Toilet Paper',
+      quantity: 100,
+      price: '15.00',
+      imageUrl:
+        'https://images-na.ssl-images-amazon.com/images/I/9133wpvx-uL._AC_SL1500_.jpg',
+      description: 'Just got real with Scott ComfortPlus Toilet paper'
+    };
+
+    beforeEach(() => {
+      return Product.create({
+        ...toiletPaper
+      });
+    });
+
+    it('GET /api/products', async () => {
+      const res = await request(app)
+        .get('/api/products')
+        .expect(200);
+
+      expect(res.body).to.be.an('array');
+      expect(res.body[0]).to.be.equal(toiletPaper);
+    });
+  }); // end describe('/api/products')
+}); // end describe('Products routes')

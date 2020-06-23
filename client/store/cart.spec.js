614c64a06f70af81207303577287b2349476cf67
Anna Morozova <anna.mik.morozova@gmail.com>
2020-06-23 16:22:26 -0400

    Style : centered title

--- client/store/cart.spec.js
+++ client/store/cart.spec.js
@@ -1,11 +1,8 @@
 import {expect} from 'chai';
-import React from 'react';
 import enzyme, {shallow} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import configureMockStore from 'redux-mock-store';
 import thunkMiddleware from 'redux-thunk';
-import waitForExpect from 'wait-for-expect';
-import {Provider} from 'react-redux';
 import * as rrd from 'react-router-dom';
 
 const {MemoryRouter} = rrd;
@@ -26,7 +23,7 @@ import {
   getCartThunk,
   updateQuantityThunk,
   removeFromCart
-} from '../store/guestCart';
+} from '../store/Cart';
 
 import store, {reducer} from '.';
 import {createStore} from 'redux';
